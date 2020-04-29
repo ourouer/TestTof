7b172a9b8b78fdab57dfa05c43f035be60415bc6
ourouer <64204654+ourouer@users.noreply.github.com>
2020-04-29 17:12:14 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,41 @@
-const PWM_ADD = 0x01
-const RGB = 0x01
+//let alreadyInit = 0
+//const PWM_ADD = 0x01
+//const MOTOR = 0x02
+//const RGB = 0x01
+
+//% weight=10 color=#FFAAFF icon="\uf1b9" block="Tof"
+//% groups='["Tests", "Mouvements"]'
 /**
- * Functions are mapped to blocks using various macros 
- * in comments starting with % (e.g., //% block).
- * The most important macro "block" specifies that a
- * block should be generated for a **exported** function.
+ *Ma premiere extension de test
  */
-//% color="#FFAAFF" icon="\uf1b9" block="Tof"
 namespace totof {
-
-    //% block
-    export function afficheTof() {
-        basic.showString("Tof")
-
+    export enum testVar {
+        //% blockId="Gauche" block="Gauche"
+        Gauche = 0,
+        //% blockId="Droite" block="Droite"
+        Droite = 1,
+        //% blockId="Avant" block="Avant"
+        Avant = 2,
+        //% blockId="Arriere" block="Arriere"
+        Arriere = 3
+    }
+    
+    //% weight=100
+    //% group="Tests"
+    //% blockId=ID_Affiche_Tof block="Set|%index|to|%Direction|at the speed|%speed" 
+    export function traductionFranToAng(testVar: Mot): void {
+       if (testVar == Mot.Gauche) { 
+            basic.showString("Left");
+       }
+       else if (testVar == Mot.Droite) { 
+            basic.showString("Right");
+       }
+       else if (testVar == Mot.Avant) { 
+            basic.showString("Forward");
+       }
+       else if (testVar == Mot.Arriere) { 
+            basic.showString("Backward");
+       }
     }
 
     // note that Caml casing yields lower case
@@ -23,7 +46,7 @@ namespace totof {
 
     }
 
-    export function setPwmRGB(red: number, green: number, blue: number): void {
+    /*export function setPwmRGB(red: number, green: number, blue: number): void {
 
         let buf = pins.createBuffer(4);
         buf[0] = RGB;
@@ -32,5 +55,5 @@ namespace totof {
         buf[3] = blue;
 
         pins.i2cWriteBuffer(PWM_ADD, buf);
-    }
+    }*/
 }
